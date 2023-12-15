2771dc530d597031e318096d6ac65ea0fdac61ab
SRIHARI-VB <112186853+SRIHARI-VB@users.noreply.github.com>
2023-12-15 18:13:36 +0530

    completed chat like structure

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const setupTextarea = document.getElementById('setup-textarea');
-const setupInputContainer1= document.getElementById('setup-input-container');
 const setupInputContainer = document.getElementById('setup-input-container');
 const setupContainer = document.getElementById('setup-container');
 const movieBossText = document.getElementById('movie-boss-text');
-const reply= document.getElementById('reply'); 
-const replyWrap= document.getElementById('reply-wrap'); 
+const chatBox = document.getElementById('chat-box');
+// const reply= document.getElementById('reply'); 
+// const replyWrap= document.getElementById('reply-wrap'); 
 const loading = document.getElementById('loading');
-const query = document.getElementById('query');
-const queryWrap = document.getElementById('query-wrap');
+// const query = document.getElementById('query');
+// const queryWrap = document.getElementById('query-wrap');
 const apiKey = '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';
 const url = 'https://ikez94vmpe.execute-api.us-east-1.amazonaws.com/v1/chatbot';
 
@@ -17,13 +17,17 @@ document.getElementById('send-btn').addEventListener('click', async () => {
 
   if (setupTextarea.value) {
     
-    queryWrap.style.border = '1px solid #000';
-    queryWrap.style.borderRadius = '10px';
-    queryWrap.style.padding = '10px';
-    queryWrap.style.width = '75%';
-    queryWrap.style.marginTop = '10px';
+    // queryWrap.style.border = '1px solid #000';
+    // queryWrap.style.borderRadius = '10px';
+    // queryWrap.style.padding = '10px';
+    // queryWrap.style.width = '75%';
+    // queryWrap.style.marginTop = '10px';
 
-    query.innerHTML = `<p>${setupTextarea.value}</p>`;
+    // query.innerHTML = `<p>${setupTextarea.value}</p>`;
+
+    chatBox.appendChild(await createQuery());
+    
+    
     
     loading.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`;
     
@@ -40,12 +44,50 @@ document.getElementById('send-btn').addEventListener('click', async () => {
   // setupInputContainer.innerHTML =`<p>dwdvdv</p>`;
   // setupTextarea.focus();/
 
-
- 
-
-
 }
 );
+
+async function createQuery() {
+  const queryChat = document.createElement('div');
+  queryChat.classList.add('query-chat');
+  queryChat.innerHTML = `<p>${setupTextarea.value}</p>`;
+  queryChat.style.border = '3px solid rgb(205, 39, 10)';
+
+  queryChat.style.borderRadius = '10px';
+  queryChat.style.padding = '10px';
+  queryChat.style.width = '75%';
+  queryChat.style.marginTop = '10px';
+  return queryChat;
+  
+  // const query = document.getElementById('query');
+
+  // query.innerHTML = ``;
+
+  // setupInputContainer.innerHTML =`<p>dwdvdv</p>`;
+  // setupTextarea.focus();
+  
+}
+
+async function createReply(replyText) {
+  const replyChat = document.createElement('div');
+  replyChat.classList.add('reply-chat');
+  replyChat.style.display = 'flex';
+  replyChat.style.justifyContent = 'flex-end';
+  replyChat.style.width = '100%';
+  replyChat.innerHTML = `<div class="reply-wrap" id="reply-wrap">
+  <p>${replyText}</p>
+  </div>`;
+
+  // const replyWrap = document.getElementById('reply-wrap');
+  // replyWrap.style.border = '1px solid #000';
+  // replyWrap.style.borderRadius = '10px';
+  // replyWrap.style.padding = '10px';
+  // replyWrap.style.width = '70%';
+  // replyWrap.style.marginTop = '10px';
+
+
+  return replyChat;
+}
 
 
 
@@ -68,13 +110,15 @@ async function fetchBotReply() {
 
     const data = await response.json();
     
-    reply.innerText = data.output.text;
+    chatBox.appendChild(await createReply(data.output.text));
+
+    // reply.innerText = data.output.text;
     
-    replyWrap.style.border = '1px solid #000';
-    replyWrap.style.borderRadius = '10px';
-    replyWrap.style.padding = '10px';
-    replyWrap.style.width = '70%';
-    replyWrap.style.marginTop = '10px';
+    // replyWrap.style.border = '1px solid #000';
+    // replyWrap.style.borderRadius = '10px';
+    // replyWrap.style.padding = '10px';
+    // replyWrap.style.width = '70%';
+    // replyWrap.style.marginTop = '10px';
 
     
     
